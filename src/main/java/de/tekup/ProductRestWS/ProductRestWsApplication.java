b4354dd21f9d2ceb1d2a752133e66a8eccc56866
ines-mouakher <ines.mouakher@fsegt.utm.tn>
2023-05-02 19:34:14 +0100

    Initial commit

---
+++ src/main/java/de/tekup/ProductRestWS/ProductRestWsApplication.java
@@ -0,0 +1,13 @@
+package de.tekup.ProductRestWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRestWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRestWsApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+--INITIALISATION TABLE PRODUIT
+INSERT INTO  Product(designation, price, quantity) values ('ordinateur', 300, 50);
+INSERT INTO Product(designation, price, quantity) values ('imprimante', 500, 20);
+INSERT INTO Product(designation, price, quantity) values ('clavier', 100, 30);
